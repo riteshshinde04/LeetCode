7d3217fc39c8545bc27e862a03c2678d0db7769f
Ritesh Shinde <114718220+riteshshinde04@users.noreply.github.com>
2024-04-11 21:11:46 +0530

    Add files via upload

---
+++ Solution 36.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(nums, ans, 0);
+        return ans;
+    }
+
+    void backtrack(int[] nums, List<List<Integer>> ans, int index) {
+
+        if (index == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int i : nums) list.add(i);
+            ans.add(list);
+
+        } else {
+            for (int i = index; i < nums.length; i++) {
+                swap(i , index, nums);
+                backtrack(nums, ans, index + 1);
+                swap(i, index, nums);
+            }
+        }
+    }
+
+    void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
