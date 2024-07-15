68ed2d6a9f370bf23f662b7082cedeeb38500367
Ritesh Shinde <114718220+riteshshinde04@users.noreply.github.com>
2024-07-15 16:12:38 +0530

    Add files via upload

---
+++ Solution 133.java
@@ -0,0 +1,29 @@
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        HashMap<Integer, TreeNode> map = new HashMap<>();
+        Set<Integer> childern = new HashSet<>();
+        for (int[] arr : descriptions) {
+            int parent = arr[0], child = arr[1], isLeft = arr[2];
+            childern.add(child);
+            TreeNode node = map.getOrDefault(parent, new TreeNode(parent));
+            if (isLeft == 1) {
+                node.left = map.getOrDefault(child, new TreeNode(child));
+                map.put(child, node.left);
+            } else {
+                node.right = map.getOrDefault(child, new TreeNode(child));
+                map.put(child, node.right);
+            }
+            map.put(parent, node);
+        }
+
+        int root = -1;
+        for (int [] arr: descriptions) {
+            if (!childern.contains(arr[0])) {
+                root = arr[0];
+                break;
+            }
+        }
+
+        return map.getOrDefault(root, null);
+    }
+}
